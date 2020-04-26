eae9537229c8a366a33c51c581a083923373f34a
unknown <macjay314@gmail.com>
2020-04-26 13:04:16 -0700

    Start of priceCalculator function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
-function Pizza(size, topping){
+function Pizza(size){
   this.size = size;
   this.toppings = [];
 }
 
-//Take in an array of toppings, and push those toppings as key value pairs to this.toppings
-//Push a toppings coverage numbe to this.toppings
-Pizza.prototype.AddToppings = function(){
+//Take in an array of toppings and coverage, and push those toppings as key value pairs to this.toppings
+//Push a toppings coverage number to this.toppings
+//each push should look like: this.toppings.push([topping: string, coverage: 1]);
+Pizza.prototype.AddToppings = function(inputT){
+  for (i = 0; i < inputT.length; i++){
+    this.toppings.push(arrayT[i]);
+  }
+}
 
+Pizza.prototype.priceCalculator = function(pizza){
+  let price;
+  let cheapToppings = ["pepperoni", "spinach", "pineapple", "jalapenos"];
+  let expensiveToppings = ["artichoke hearts", "olives", "shredded beef"];
+  price += this.size;
+  for(i = 0; i < this.toppings.length; i++)
 }
