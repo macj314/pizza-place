43f90af55832788c738e3c42f55d205924a68e4f
unknown <macjay314@gmail.com>
2020-04-26 14:03:06 -0700

    First attempt at collecting user data

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,36 @@ function Pizza(size){
 //Take in an array of toppings and coverage, and push those toppings as key value pairs to this.toppings
 //Push a toppings coverage number to this.toppings
 //each push should look like: this.toppings.push([topping: string, coverage: 1]);
-Pizza.prototype.AddToppings = function(inputT){
-  for (i = 0; i < inputT.length; i++){
-    this.toppings.push(arrayT[i]);
-  }
-}
+// Pizza.prototype.addToppings = function(inputT){
+//     this.toppings.push(inputT);
+// }
 
-Pizza.prototype.priceCalculator = function(pizza){
+Pizza.prototype.priceCalculator = function(pie){
   let price;
   let cheapToppings = "pepperoni spinach pineapple jalapenos";
-  let expensiveToppings = ["artichoke hearts", "olives", "shredded beef"];
-  price += this.size;
-  for(i = 0; i < this.toppings.length; i++){
-    if(this.toppings[i].includes(cheapToppings)){
+  let expensiveToppings = ["artichoke", "olives", "beef"];
+  price += pie.size;
+  for(i = 0; i < pie.toppings.length; i++){
+    if(pie.toppings[i].includes(cheapToppings)){
       price += 1;
-    } else if (this.toppings[i].includes(expensiveToppings)){
+    } else if (pie.toppings[i].includes(expensiveToppings)){
       price += 2;
     }
   }
   return price;
 }
+
+$(document).ready(function(){
+  $("#submit").click(function(event){
+    event.preventDefault();
+    // let inputToppings = $("input:checkbox[name=toppings]:checked").val;
+    // console.log(inputToppings);
+    let inputSize = $("input:radio[name=size]:checked").val();
+    console.log(inputSize);
+    let pizza = new Pizza(inputSize);
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      pizza.toppings.push($(this).val());
+    });
+    console.log(pizza);
+  })
+})
